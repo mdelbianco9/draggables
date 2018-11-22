9f5071f9816749e4c3058301c75592609f8e3d54
marcus delbianco <mdelbianco9@gmail.com>
2018-11-22 01:12:22 -0800

    Added an onkeypress event to move slider

--- js/slider.js
+++ js/slider.js
@@ -9,6 +9,9 @@ thumb.onmousedown = function(event){
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
+    var demo = document.getElementById('demo');
+
+
 
     function onMouseMove(event){
         newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
@@ -25,13 +28,33 @@ thumb.onmousedown = function(event){
 
         thumb.style.left = newLeft + "px";
         console.log(newLeft)
+        demo.innerHTML = Math.floor((newLeft/(slider.getBoundingClientRect().width-20))*100);
+
+        console.log(slider.getBoundingClientRect())
     }
 
     function onMouseUp(){
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mouseup', onMouseUp);
     }
-
-    
-
 }
+var count = 0;
+document.onkeydown = function(event){
+    var demo = document.getElementById('demo');
+    if(event.keyCode == 39){
+        let thumb = slider.querySelector('.thumb');
+        count=count + 10;
+    }else if(event.keyCode == 37){
+        count=count-10;
+    }
+    thumb.style.left = count + "px";
+    demo.innerHTML = count
+}
+
+
+
+
+
+
+
+
